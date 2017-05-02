cc3722f765c8dac69043ff49f53c6b86a9c08754
Chris Parsons <christopherdotparsons@gmail.com>
2017-05-02 20:09:04 +0000

    Added main python file to run the bot

---
+++ krispybot.py
@@ -0,0 +1,11 @@
+import json
+import requests
+
+api_token = "get this from home"
+telegram_api_url = "get this from home"
+
+def get_last_message(updates):
+	
+
+
+#piecemeal this code.  i want to see what the updates look like first and then parse through them so i can write my own function!  wish i had the cloud srver though =(
